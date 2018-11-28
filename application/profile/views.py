f2fe9886c7e8c51444eb1c1c9ec127043c261558
Roni Lindholm <roni.lindholm@gmail.com>
2018-11-28 21:39:36 +0200

    Fix error handling on view profile

--- application/profile/views.py
+++ application/profile/views.py
@@ -6,14 +6,11 @@ from application.auth.models import User
 
 @app.route("/profile/<profile_id>", methods=["GET"])
 def view_profile(profile_id):
-    error = []
-    if not profile_id:
-        error.append("Käyttäjätunnusta ei löytynyt")
-        return render_template("profile/view.html", user=None, errors=error)
+    error = None
     p = User.query.get(profile_id)
     
     if not p:
-        error.append("Käyttäjätunnusta ei löytynyt")
+        error ="Käyttäjätunnusta ei löytynyt."
         return render_template("profile/view.html", user=None, errors=error)
     
     return render_template("profile/view.html", user=p, errors=error)
