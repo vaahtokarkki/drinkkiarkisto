8a380cd0a08a8601a49be6d0e5165959542e4bce
Roni Lindholm <roni.lindholm@gmail.com>
2018-11-30 23:10:43 +0200

    Change stats to show only published records

--- application/views.py
+++ application/views.py
@@ -9,8 +9,8 @@ from application.keywords.models import Keyword
 @app.route("/")
 def index():
     stats = {
-        "drinks": len(Drink.query.all()),
-        "ingredients": len(Ingredient.query.all()),
-        "keywords": len(Keyword.query.all())
+        "drinks": len(Drink.query.filter(Drink.accepted==True).all()),
+        "ingredients": len(Ingredient.query.filter(Ingredient.accepted==True).all()),
+        "keywords": len(Keyword.query.filter(Keyword.accepted==True).all())
     }
     return render_template("index.html", form=SearchForm(), stats=stats)
